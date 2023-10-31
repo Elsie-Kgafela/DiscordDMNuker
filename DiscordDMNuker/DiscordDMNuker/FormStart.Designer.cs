3ba5deb2e8b2bdbb59aad8272060ee8d893052dd
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:21:03 +0200

    update file

---
+++ DiscordDMNuker/DiscordDMNuker/FormStart.Designer.cs
@@ -0,0 +1,256 @@
+
+namespace DiscordDMNuker
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            checkBox1 = new System.Windows.Forms.CheckBox();
+            checkBox2 = new System.Windows.Forms.CheckBox();
+            checkBox3 = new System.Windows.Forms.CheckBox();
+            checkBox4 = new System.Windows.Forms.CheckBox();
+            checkBox5 = new System.Windows.Forms.CheckBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            checkBox6 = new System.Windows.Forms.CheckBox();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            textBox5 = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(15, 27);
+            textBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(182, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(37, 10);
+            label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(126, 15);
+            label2.TabIndex = 14;
+            label2.Text = "Guild/Group/Victim ID";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(76, 91);
+            label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(38, 15);
+            label1.TabIndex = 16;
+            label1.Text = "Token";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(16, 107);
+            textBox2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(180, 23);
+            textBox2.TabIndex = 17;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(7, 383);
+            button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(186, 35);
+            button1.TabIndex = 18;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(16, 191);
+            checkBox1.Margin = new System.Windows.Forms.Padding(2);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(118, 19);
+            checkBox1.TabIndex = 21;
+            checkBox1.Text = "Save Images/Vids";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new System.Drawing.Point(16, 215);
+            checkBox2.Margin = new System.Windows.Forms.Padding(2);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new System.Drawing.Size(104, 19);
+            checkBox2.TabIndex = 22;
+            checkBox2.Text = "Save Messages";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new System.Drawing.Point(16, 240);
+            checkBox3.Margin = new System.Windows.Forms.Padding(2);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new System.Drawing.Size(113, 19);
+            checkBox3.TabIndex = 23;
+            checkBox3.Text = "Delete Messages";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new System.Drawing.Point(16, 264);
+            checkBox4.Margin = new System.Windows.Forms.Padding(2);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new System.Drawing.Size(98, 19);
+            checkBox4.TabIndex = 24;
+            checkBox4.Text = "Is Group Chat";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new System.Drawing.Point(15, 312);
+            checkBox5.Margin = new System.Windows.Forms.Padding(2);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new System.Drawing.Size(100, 19);
+            checkBox5.TabIndex = 25;
+            checkBox5.Text = "Edit Messages";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(8, 334);
+            textBox3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(185, 47);
+            textBox3.TabIndex = 26;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new System.Drawing.Point(16, 288);
+            checkBox6.Margin = new System.Windows.Forms.Padding(2);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new System.Drawing.Size(81, 19);
+            checkBox6.TabIndex = 27;
+            checkBox6.Text = "Is Channel";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new System.Drawing.Point(16, 67);
+            textBox4.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(181, 23);
+            textBox4.TabIndex = 29;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(65, 51);
+            label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(65, 15);
+            label4.TabIndex = 28;
+            label4.Text = "Channel ID";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new System.Drawing.Point(13, 147);
+            textBox5.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(180, 23);
+            textBox5.TabIndex = 31;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(73, 131);
+            label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(37, 15);
+            label3.TabIndex = 30;
+            label3.Text = "Proxy";
+            // 
+            // FormStart
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(197, 425);
+            Controls.Add(textBox5);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(checkBox6);
+            Controls.Add(textBox3);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Margin = new System.Windows.Forms.Padding(2);
+            Name = "FormStart";
+            Text = "Nuke";
+            Load += FormStart_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+    }
+}
